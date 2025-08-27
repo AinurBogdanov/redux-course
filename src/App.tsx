e2e793558c57f3a1fb9c108f95853856e2d1391a
AinurWin <ainurbogdanov69@gmail.com>
2025-08-27 10:50:23 +0400

    set up first exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,13 @@
-import './App.css';
-import { useDispatch } from 'react-redux';
-import {
-  selectCounter,
-  useAppSelector,
-  type CounterId,
-  type DecrementAction,
-  type IncrementAction,
-} from './store';
+import { Counters } from './counters';
+import { UsersList } from './users-list';
 
 function App() {
   return (
-    <>
-      <Counter counterId="first" />
-      <Counter counterId="second" />
-    </>
+    <div className="container p-5 flex flex-col gap-5">
+      <Counters />
+      <UsersList />
+    </div>
   );
 }
-
-const Counter = ({ counterId }: { counterId: CounterId }) => {
-  const dispatch = useDispatch();
-  const currentCounter = useAppSelector((state) => selectCounter(state, counterId));
-  console.log(`counter updated ${counterId}`);
-
-  // const [, forceUpdate] = useReducer((x) => x + 1, 0);
-  // const lastCounterRef = useRef<ReturnType<typeof selectCounter>>(null);
-
-  // useEffect(() => {
-  //   const unsubscribe = store.subscribe(() => {
-  //     const currentCounter = selectCounter(store.getState(), counterId);
-  //     const lastCounter = lastCounterRef.current;
-  //     if (lastCounter !== currentCounter) forceUpdate();
-
-  //     lastCounterRef.current = currentCounter;
-  //   });
-  //   return unsubscribe;
-  // }, []);
-  // const counterState = selectCounter(store.getState(), counterId);
-
-  return (
-    <>
-      counter {currentCounter?.counter}
-      <button
-        onClick={() =>
-          dispatch({ type: 'increment', payload: { counterId } } satisfies IncrementAction)
-        }
-      >
-        increment
-      </button>
-      <button
-        onClick={() =>
-          dispatch({ type: 'decrement', payload: { counterId } } satisfies DecrementAction)
-        }
-      >
-        decrement
-      </button>
-    </>
-  );
-};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './store.ts';
 
-createRoot(document.getElementById('root')!).render(
-  //STRICT MODE useEffects rut two times
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 );
